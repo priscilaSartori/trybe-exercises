7b9037022b886271d5c6da8f3e14303b6a9f06d1
Priscila <priscilasartori2@gmail.com>
2022-09-08 22:49:23 -0300

    Requisito 3

--- apiCoins.js
+++ apiCoins.js
@@ -1,5 +1,5 @@
 const fetchCoins = async () => {
-    const url = `https://api.coincap.io/v2/assets`;
+    const url = 'https://api.coincap.io/v2/assets';
 
     const result = await fetch(url)
     .then((response) => response.json())
@@ -8,12 +8,13 @@ const fetchCoins = async () => {
 
     return result;
 };
-fetchCoins()
 
 const setCoins = async () => {
     const coins = await fetchCoins();
     const coinsList = document.getElementById('coins-list');
-    coins.forEach((coin) => {
+    coins
+    .slice(0, 10)
+    .forEach((coin) => {
       const newLi = document.createElement('li');
       const usdPrice = Number(coin.priceUsd);
       newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
@@ -21,4 +22,3 @@ const setCoins = async () => {
     });
   };
   window.onload = () => setCoins();
- 
