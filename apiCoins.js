6ba527eca6d548bfe63cf2549254e6a507a79f4a
Priscila <priscilasartori2@gmail.com>
2022-09-08 22:30:25 -0300

    Requisito 1

--- apiCoins.js
+++ apiCoins.js
@@ -1,4 +1,11 @@
 const fetchCoins = async () => {
-};
+    const url = `https://api.coincap.io/v2/assets`;
 
-fetchCoins();
+    const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => `Algo deu errado :( \n${error}`);
+
+    return result;
+};
+fetchCoins()
