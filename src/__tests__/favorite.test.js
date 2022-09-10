d7c7f4a7dbc00c1dfb7ccaf6b128fd1ef5435f25
Priscila <priscilasartori2@gmail.com>
2022-09-10 00:03:43 -0300

    Requisito 3.1

--- src/__tests__/favorite.test.js
+++ src/__tests__/favorite.test.js
@@ -1,8 +1,8 @@
-// const { saveFavoriteMagicCard } = require('../magic');
-// const favoriteCards = require('../data/favoriteCards');
+const { saveFavoriteMagicCard } = require('../magic');
+const favoriteCards = require('../data/favoriteCards');
 
 const restoreFavoriteCards = () => {
-  // implemente seu código aqui
+   favoriteCards.splice(4, favoriteCards.length);
 };
 
 jest.setTimeout(10000);
@@ -11,23 +11,26 @@ describe('3 - Testes da função saveFavoriteMagicCard', () => {
   afterEach(restoreFavoriteCards);
 
   it('3.1 Testa se um novo card é adicionado a cada execução', async () => {
-    // expect.assertions();
-    // await saveFavoriteMagicCard('130553');
-    // implemente seus testes aqui
+    expect.assertions();
+    await saveFavoriteMagicCard('130553');
+    expect(favoriteCards).toHaveLength(5);
+    expect(favoriteCards[4].name).toBe('Beacon of Immortality');
 
+    await saveFavoriteMagicCard('130553');
+    expect(favoriteCards.length).toBe(6);
   });
 
-  it('3.2 Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
-    // expect.assertions();
-    // expect(favoriteCards).toHaveLength(4);
-  });
-});
+//   it('3.2 Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
+//     // expect.assertions();
+//     // expect(favoriteCards).toHaveLength(4);
+//   });
+// });
 
-describe('4 - Testes da função saveFavoriteMagicCard', () => {
-  it(`4.1 Implemente um teste que verifique que o array que você obteve com o map
-  contém todos os quatro nomes iniciais nomes`, async () => {
-    // expect.assertions();
-    // implemente seus testes aqui
+// describe('4 - Testes da função saveFavoriteMagicCard', () => {
+//   it(`4.1 Implemente um teste que verifique que o array que você obteve com o map
+//   contém todos os quatro nomes iniciais nomes`, async () => {
+//     // expect.assertions();
+//     // implemente seus testes aqui
 
-  });
+//   });
 });
