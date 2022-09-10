da417b43d92adbefcd44e7014f4997e6306a3584
Priscila <priscilasartori2@gmail.com>
2022-09-09 23:29:35 -0300

    Requisito 1.3

--- src/__tests__/magic.test.js
+++ src/__tests__/magic.test.js
@@ -13,15 +13,15 @@ describe('1 - Testes da função getMagicCard', () => {
     expect(fetch).toHaveBeenCalled();  
   });
 
-//   it('1.3 Verifique se a função fetch foi chamada com o endpoint correto', async () => {
-//     // await getMagicCard('130550');
-//     // implemente seus testes aqui
-//   });
-// });
+  it('1.3 Verifique se a função fetch foi chamada com o endpoint correto', async () => {
+    await getMagicCard('130550');
+    expect(fetch).toHaveBeenCalledWith('https://api.magicthegathering.io/v1/cards/130550');
+  });
+});
 
 // describe('2 - Testando o retorno da função getMagicCard', () => {
 //   it('2.1 Verifique se a carta retornada possui o nome Ancestor\'s Chosen.', async () => {
 //     // const response = await getMagicCard('130550');
 //     // implemente seus testes aqui
-  });
+  // });
 // });
