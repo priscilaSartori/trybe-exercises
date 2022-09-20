b083f16687bb4e1a721a37d97330862f47f5d3ea
Priscila <priscilasartori2@gmail.com>
2022-09-19 22:46:21 -0300

    Requisito extra

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
-    <h1>Exercise - Frontend content</h1>
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-        <header>
-      <h1 className='title'>Conteúdos de Frontend</h1>
+      <header>
+        <h1 className="title">Conteúdos de Frontend</h1>
       </header>
     );
   }
