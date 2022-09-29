d52a40fe7cee22d5c020fcd72c1ce9c149a1574a
Priscila <priscilasartori2@gmail.com>
2022-09-29 20:58:22 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      imageUrl: '',
+    };
+  }
+
   componentDidMount() {
-    // Implemente sua lógica aqui
+    this.fetchDog();
   }
 
   shouldComponentUpdate(/* nextProps, nextState */) {
@@ -12,12 +21,27 @@ class App extends React.Component {
   }
 
   componentDidUpdate() {
-    
+
     // Implemente sua lógica aqui
   }
 
+  fetchDog = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((data) => this.setState({ imageUrl: data.message, isLoading: false }));
+  };
+
   render() {
-    return <h1>Doguinhos</h1>;
+    const { imageUrl, isLoading } = this.state;
+    if (isLoading) return <h2>Loading...</h2>;
+    return (
+      <div>
+        <h1>Dog image - Imagens de Doguinhos</h1>
+        <p>O doguinho selecionado é:</p>
+        <img src={ imageUrl } alt="Doguinho aleatório" />
+        <button type="button" onClick={ this.fetchDog }>Novo doguinho!</button>
+      </div>
+    );
   }
 }
 
