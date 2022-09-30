34d31ac911ab272551dc67a4ddc8efe94c488674
Priscila <priscilasartori2@gmail.com>
2022-09-30 20:36:17 -0300

    iniciando o exercício pokedex-router

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import Pokedex from './components/Pokedex';
 function App() {
   return (
     <div className="App">
-      <Pokedex
-        pokemonList={ pokemonList }
-      />
+      <Pokedex pokemonList={ pokemonList } />
+
     </div>
   );
 }
