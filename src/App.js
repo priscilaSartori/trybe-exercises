2f5236af9ac21d7978a362fc10ca04a2663839fd
Priscila <priscilasartori2@gmail.com>
2022-09-30 21:13:47 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import './App.css';
-import pokemonList from './data';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import pokemonListData from './data';
 import Pokedex from './components/Pokedex';
+import About from './components/About';
 
-function App() {
-  return (
-    <div className="App">
-      <Pokedex pokemonList={ pokemonList } />
+class App extends Component {
+  constructor() {
+    super();
 
-    </div>
-  );
+    this.state = {
+      pokemonList: pokemonListData,
+    };
+  }
+
+  render() {
+    const { pokemonList } = this.state;
+
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <Pokedex pokemonList={ pokemonList } />
+            ) }
+          />
+          <Route path="/about" component={ About } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
